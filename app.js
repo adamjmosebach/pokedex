775349d8c93e2be18ad11cc895e8aaccce4a57f1
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-07-15 17:50:58 -0400

    cleaned up errant skewer cases

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const form = document.querySelector('#nameNumSelect')
 function displayData(pokeData) {
 
   //Display main information & image
-  const card = document.querySelector('#left-panel');
-  card.classList = 'poke-card';
+  const card = document.querySelector('#leftPanel');
+  card.classList = 'pokeCard';
   const pokeNum = document.createElement('h2');
   pokeNum.innerText = `# ${pokeData.id}`;
   const pokeName = document.createElement('h1');
@@ -59,40 +59,40 @@ function displayData(pokeData) {
   typesDiv.append(pokeTypeHeader, pokeTypeList);
 
   //Good-Week Section
-  const goodWeak = document.querySelector('#good-weak');
+  const goodWeak = document.querySelector('#goodWeak');
 
   //Add good button & hidden display area
   const goodButtonCreate = document.createElement('div');
-  goodButtonCreate.classList = 'battle-button';
-  goodButtonCreate.id = 'good-button';
+  goodButtonCreate.classList = 'battleButton';
+  goodButtonCreate.id = 'goodButton';
   goodButtonCreate.innerText = 'Good Against';
   const goodDisplayCreate = document.createElement('div');
-  goodDisplayCreate.id = 'good-display';
-  goodDisplayCreate.classList = 'display-hidden';
+  goodDisplayCreate.id = 'goodDisplay';
+  goodDisplayCreate.classList = 'displayHidden';
   goodWeak.append(goodButtonCreate, goodDisplayCreate);
 
   //Event listener for good button
-  const goodButton = document.querySelector('#good-button');
+  const goodButton = document.querySelector('#goodButton');
   goodButton.addEventListener('click', () => {
-    const goodDisplay = document.querySelector('#good-display');
-    goodDisplay.classList.toggle('display-hidden');
+    const goodDisplay = document.querySelector('#goodDisplay');
+    goodDisplay.classList.toggle('displayHidden');
   });
 
   //Add weak button & hidden display area
   const weakButtonCreate = document.createElement('div');
-  weakButtonCreate.classList = 'battle-button';
-  weakButtonCreate.id = 'weak-button';
+  weakButtonCreate.classList = 'battleButton';
+  weakButtonCreate.id = 'weakButton';
   weakButtonCreate.innerText = 'Weak Against';
   const weakDisplayCreate = document.createElement('div');
-  weakDisplayCreate.id = 'weak-display';
-  weakDisplayCreate.classList = 'display-hidden';
+  weakDisplayCreate.id = 'weakDisplay';
+  weakDisplayCreate.classList = 'displayHidden';
   goodWeak.append(weakButtonCreate, weakDisplayCreate);
 
   //Event listener for weak button
-  const weakButton = document.querySelector('#weak-button');
+  const weakButton = document.querySelector('#weakButton');
   weakButton.addEventListener('click', () => {
-    const weakDisplay = document.querySelector('#weak-display');
-    weakDisplay.classList.toggle('display-hidden');
+    const weakDisplay = document.querySelector('#weakDisplay');
+    weakDisplay.classList.toggle('displayHidden');
   });
 
   //Display: Type(s) / Good against / Weak against
@@ -119,16 +119,16 @@ async function goodWeakOutcomes(pokeType) {
         const bestAgainstHeader = document.createElement('h4');
         bestAgainstHeader.innerText = `${pokeType} types are best against:`;
         const bestList = document.createElement('ul');
-        bestList.id = `best-against-${pokeType}`;
-        const goodSection = document.querySelector('#good-display');
+        bestList.id = `bestAgainst${pokeType}`;
+        const goodSection = document.querySelector('#goodDisplay');
         goodSection.append(bestAgainstHeader, bestList);
 
         //Sets up 'Weak Against' section
         const weakestAgainstHeader = document.createElement('h4');
         weakestAgainstHeader.innerText = `${pokeType} types are weakest against:`;
         const weakestList = document.createElement('ul');
-        weakestList.id = `weak-against-${pokeType}`;
-        const weakSection = document.querySelector('#weak-display');
+        weakestList.id = `weakAgainst${pokeType}`;
+        const weakSection = document.querySelector('#weakDisplay');
         weakSection.append(weakestAgainstHeader, weakestList);
 
         //Gets each type's url and passes it to getTypeData
@@ -153,7 +153,7 @@ async function getTypeData(typeUrl, pokeType) {
       for (let i = 0; i < bestAgainstArr.length; i++) {
         const bestAgainstItem = document.createElement('li');
         bestAgainstItem.textContent = `${bestAgainstArr[i].name}`;
-        const bestList = document.querySelector(`#best-against-${pokeType}`);
+        const bestList = document.querySelector(`#bestAgainst${pokeType}`);
         bestList.append(bestAgainstItem);
       }
     } else {
@@ -162,7 +162,7 @@ async function getTypeData(typeUrl, pokeType) {
       for (let i = 0; i < secondBestAgainstArr.length; i++) {
         const secondBestAgainstItem = document.createElement('li');
         secondBestAgainstItem.textContent = `${secondBestAgainstArr[i].name}`;
-        const bestList = document.querySelector(`#best-against-${pokeType}`);
+        const bestList = document.querySelector(`#bestAgainst${pokeType}`);
         bestList.append(secondBestAgainstItem);
       }
     }
@@ -172,7 +172,7 @@ async function getTypeData(typeUrl, pokeType) {
     for (let i = 0; i < weakestAgainstArr.length; i++) {
       const weakestAgainstItem = document.createElement('li');
       weakestAgainstItem.textContent = `${weakestAgainstArr[i].name}`;
-      const weakestList = document.querySelector(`#weak-against-${pokeType}`);
+      const weakestList = document.querySelector(`#weakAgainst${pokeType}`);
       weakestList.append(weakestAgainstItem);
     }
 
@@ -212,8 +212,8 @@ async function getData(input) {
   catch (err) {
     console.log('My error is: ' + err);
     //Remove previous style
-    const display = document.querySelector('#left-panel');
-    display.classList.toggle('poke-card',false);
+    const display = document.querySelector('#leftPanel');
+    display.classList.toggle('pokeCard',false);
     //Display error message if pokemon not found
     const errorDivCreate = document.createElement('div');
     errorDivCreate.id = 'errorDiv';
@@ -248,7 +248,7 @@ function removeData() {
   while (typesDiv.lastChild) {
     typesDiv.lastChild.remove()
   }
-  const goodWeak = document.querySelector('#good-weak');
+  const goodWeak = document.querySelector('#goodWeak');
   while (goodWeak.lastChild) {
     goodWeak.lastChild.remove()
   }
@@ -270,5 +270,5 @@ form.addEventListener('submit', (e) => {
 const ball = document.querySelector('#openingPage');
 ball.addEventListener('click', () => {
   document.querySelector('#theme').play()
-  ball.classList = 'display-hidden';
+  ball.classList = 'displayHidden';
 });
